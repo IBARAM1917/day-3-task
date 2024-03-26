6b677092f21a5eb1f13ad1e741c309785ccbbefc
IBARAM1917 <abiraminagaraj22@gmail.com>
2024-03-26 14:00:46 +0530

    updated

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //1. how to compare two json have the same properties without order
+var obj1 = {name:"person1",age:5};
+var obj2 = {age:5,name:"person1"};
+var flag=true;
 
-let obj1 = {name:"person1",age:5};
-let obj2 = {age:5,name:"person1"};
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("is object equal" + flag);
 
-if(JSON.stringify(obj1,Object.keys(obj1)).length ===  JSON.stringify(obj2,Object.keys(obj2)).length)
-{
-    console.log("The JSON objects are equal");
-}
-else
-{
-    console.log("The JSON objects are not equal");
-}
 //2.display the country flags in the console
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all")
@@ -22,7 +30,7 @@ request.onload=function() {
         console.log(result[i].flags.png)
     }
     
-}
+};
 //3.use the same rest countrys and print all country names,regions ,sub-regionand populations
 var request1 = new XMLHttpRequest();
 request1.open("GET","https://restcountries.com/v3.1/all")
